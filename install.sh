b3e7c77778cc1143b5ad4f97d181fcf47adfc729
kiennt048 <54541345+kiennt048@users.noreply.github.com>
2026-01-27 17:13:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# THÔNG TIN CẤU HÌNH
+# Dừng script ngay lập tức nếu bất kỳ lệnh nào bị lỗi
+set -e
+
 GITHUB_USER="kiennt048"
 GITHUB_REPO="net-shim"
 BRANCH="main"
@@ -9,22 +11,20 @@ TOKEN="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"
 BASE_URL="https://raw.githubusercontent.com/$GITHUB_USER/$GITHUB_REPO/$BRANCH"
 HEADER="Authorization: token $TOKEN"
 
-echo ">>> [1/4] Dang tai file tu GitHub qua curl..."
+echo ">>> [1/4] Dang tai file..."
 
-# Tai Binary
-curl -H "$HEADER" -sSfL -o /usr/local/sbin/net-shim "$BASE_URL/net-shim"
+# Sử dụng -S để hiện lỗi nếu tải thất bại
+curl -H "$HEADER" -SL -o /usr/local/sbin/net-shim "$BASE_URL/net-shim"
+curl -H "$HEADER" -SL -o /usr/local/etc/rc.d/net-shim "$BASE_URL/net-shim.rc"
 
-# Tai Service Script
-curl -H "$HEADER" -sSfL -o /usr/local/etc/rc.d/net-shim "$BASE_URL/net-shim.rc"
-
-echo ">>> [2/4] Phan quyen thuc thi..."
+echo ">>> [2/4] Phan quyen..."
 chmod +x /usr/local/sbin/net-shim
 chmod +x /usr/local/etc/rc.d/net-shim
 
 echo ">>> [3/4] Dang ky service..."
 sysrc -f /etc/rc.conf.local net_shim_enable="YES"
 
-echo ">>> [4/4] Khoi chay ung dung..."
+echo ">>> [4/4] Khoi chay..."
 service net-shim restart || service net-shim start
 
 echo ">>> TRIEN KHAI HOAN TAT!"
