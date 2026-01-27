53fd46db40236dac6d8b8c9fcebfd5e6a531dd3c
kiennt048 <54541345+kiennt048@users.noreply.github.com>
2026-01-27 17:05:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
 
-# CẤU HÌNH GITHUB
-GITHUB_USER="TenUserCuaAnh"
-GITHUB_REPO="TenRepo"
+# THÔNG TIN CẤU HÌNH
+GITHUB_USER="kiennt048"
+GITHUB_REPO="net-shim"
 BRANCH="main"
-# Token dạng ghp_xxxxxxxxxxxxxxx
-GITHUB_TOKEN="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"
+TOKEN="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"
 
-# Đường dẫn Raw của GitHub
-BASE_URL="https://raw.githubusercontent.com/$GITHUB_USER/$GITHUB_REPO/$BRANCH"
+# Cấu trúc URL có nhúng Token để bypass lỗi --header
+BASE_URL="https://$TOKEN@raw.githubusercontent.com/$GITHUB_USER/$GITHUB_REPO/$BRANCH"
 
-# ĐƯỜNG DẪN TRÊN PFSENSE
-BIN_PATH="/usr/local/sbin/net-shim"
-RC_PATH="/usr/local/etc/rc.d/net-shim"
+echo ">>> Dang tai file tu GitHub cho pfSense 2.8.1..."
 
-echo ">>> [1/4] Downloading files from GitHub..."
-# Lưu ý Header khác: "Authorization: token ..." thay vì "PRIVATE-TOKEN"
-fetch -q -o $BIN_PATH --no-verify-peer --header "Authorization: token $GITHUB_TOKEN" "$BASE_URL/net-shim"
-fetch -q -o $RC_PATH --no-verify-peer --header "Authorization: token $GITHUB_TOKEN" "$BASE_URL/net-shim.rc"
+# Tai Binary
+fetch -q -o /usr/local/sbin/net-shim "$BASE_URL/net-shim"
 
-echo ">>> [2/4] Setting permissions..."
-chmod +x $BIN_PATH
-chmod +x $RC_PATH
+# Tai Service Script
+fetch -q -o /usr/local/etc/rc.d/net-shim "$BASE_URL/net-shim.rc"
 
-echo ">>> [3/4] Enabling service..."
+# Phan quyen
+chmod +x /usr/local/sbin/net-shim
+chmod +x /usr/local/etc/rc.d/net-shim
+
+# Kich hoat va Chay (Dùng sysrc để đảm bảo an toàn cho rc.conf.local)
 sysrc -f /etc/rc.conf.local net_shim_enable="YES"
-
-echo ">>> [4/4] Starting service..."
 service net-shim restart || service net-shim start
+
+echo ">>> Hoan tat trien khai net-shim!"
