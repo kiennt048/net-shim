0880e70096c7a77dbbcc5e261de361c8f4f87b9e
kiennt048 <54541345+kiennt048@users.noreply.github.com>
2026-01-28 16:10:59 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ HEALTH_URL="http://127.0.0.1:8080/health"
 RESTAPI_PKG_URL="https://github.com/pfrest/pfSense-pkg-RESTAPI/releases/latest/download/pfSense-2.8.1-pkg-RESTAPI.pkg"
 
 # 🔐 UPDATE THIS AFTER EACH BUILD
-EXPECTED_SHA256="206fa74359bd171dfdd839f2808f111e6814ff5b7921a71567e7e3fa32ccf12e"
+EXPECTED_SHA256="26de359bcc1a9ffce21d424441dab487a849cf8fca7dd57e38d30d93c7e0fb68"
 ### ==================
 
 TMPDIR="/tmp/netshim.$$"
