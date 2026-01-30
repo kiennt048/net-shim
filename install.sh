90d5af3ef54cbc17992da6f9e9ae40459dcdb990
kiennt048 <kiennt048@gmail.com>
2026-01-30 09:16:03 +0700

    Release v1.7.1.25_20260130_0916 - Auto-deployed

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ HEALTH_URL="http://127.0.0.1:8080/health"
 RESTAPI_PKG_URL="https://github.com/pfrest/pfSense-pkg-RESTAPI/releases/latest/download/pfSense-2.8.1-pkg-RESTAPI.pkg"
 
 # 🔐 UPDATE THIS AFTER EACH BUILD
-EXPECTED_SHA256="ad8dd634f48f86ba3d01e97d70823d0c97787e2a285cb67868af451b00a1b61e"
+EXPECTED_SHA256="c28c1020c27728b3b7897a4c46b4843856e26ce8c22ab66c1b27bad299749019"
 ### ==================
 
 TMPDIR="/tmp/netshim.$$"
